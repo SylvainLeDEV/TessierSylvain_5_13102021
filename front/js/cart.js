b63d12df4c81f1df83f9401a4cecb96391ac1c86
Sylvain <dev.tess.sylvain@gmail.com>
2021-10-27 13:50:02 +0200

    Ajout function deleteItem / Bug quantité a régler

--- front/js/cart.js
+++ front/js/cart.js
@@ -1,16 +1,21 @@
 window.onload = function () {
+
     const product = document.querySelector("#cart__items")
+
+
+    //On récupère les information du localStorage
     if (typeof localStorage != 'undefined' && localStorage.getItem("dataPannier") != null) {
         dataPannier = JSON.parse(localStorage.getItem("dataPannier"));
-        console.log(dataPannier)
+        console.log("data pannier : ", dataPannier)
     }
+
 
     const pannierDisplay = () => {
 
         product.innerHTML = dataPannier.map((pannier) =>
 
             `
-        <article class="cart__item" data-id="${pannier._id}">
+        <article class="cart__item" data-id="${pannier._id}" data-color="${pannier.color}">
     <div class="cart__item__img">
         <img src="${pannier.img}" alt="${pannier.alt}">
     </div>
@@ -21,7 +26,7 @@ window.onload = function () {
         </div>
         <div class="cart__item__content__settings">
             <div class="cart__item__content__settings__quantity">
-                <p>Qté : </p>
+                <p>Qté : ${pannier.quantity}</p>
                 <input type="number" class="itemQuantity" name="itemQuantity" min="1" max="100" value="${pannier.quantity}">
             </div>
             <div class="cart__item__content__settings__delete">
@@ -34,15 +39,38 @@ window.onload = function () {
         ).join(" ")
 
     }
-
-
     pannierDisplay();
 
+    const functionDeleteItem = (e) => {
 
-    const supprimer = document.querySelectorAll(".deleteItem")[0];
-    supprimer.addEventListener('click', storage);
+        const getId = e.path[4].getAttribute("data-id")
+        const getColor = e.path[4].getAttribute("data-color")
+        console.log(e)
+        console.log(getId + getColor)
+        for (const x of dataPannier) {
+            if (getId === x._id && getColor === x.color) {
+                dataPannier.splice(x, 1)
+                localStorage.setItem("dataPannier", JSON.stringify(dataPannier))
+                pannierDisplay();
+                functionAddEventListener()
+            }
+        }
+    }
 
-    function storage(event) {
+    const functionAddEventListener = () => {
+        const deleteItem = document.querySelectorAll('p.deleteItem')
+        console.log(deleteItem)
+
+        for (let i = 0; i < deleteItem.length; i++) {
+            deleteItem[i].addEventListener('click', (e) => {
+                functionDeleteItem(e);
+            })
+        }
+    }
+    functionAddEventListener();
+
+    //Fonction pour remove les canapé du local.storage + rechercher la page
+    function storageRemove(event) {
         localStorage.removeItem("dataPannier");
         window.location.reload();
         console.log(event)
