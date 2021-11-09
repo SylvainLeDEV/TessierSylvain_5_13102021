0586f7da3d52423341e7d67952553765f91d5481
Sylvain <dev.tess.sylvain@gmail.com>
2021-11-09 14:52:22 +0100

    Fin du projet (Faire vérifier par mentor si tout est OK)

--- front/js/confirmation.js
+++ front/js/confirmation.js
@@ -1,33 +1,40 @@
-if (typeof dataPannier !== "undefined"){
-let orderId;
-let searchParams = new URLSearchParams(window.location.search)
-if (searchParams.has("_orderId")) {
-    orderId = searchParams.get("_orderId")
-    console.log(" L'id du récupéré dans l'URL : ", orderId)
-}
+if (JSON.parse(localStorage.getItem("dataPannier")) !== null) {
+    let orderId;
+    let searchParams = new URLSearchParams(window.location.search)
+    if (searchParams.has("_orderId")) {
+        orderId = searchParams.get("_orderId")
+        console.log(" L'id du récupéré dans l'URL : ", orderId)
+    }
 
 
-const displayOrderId = document.getElementById("orderId");
-displayOrderId.textContent = orderId;
+    const displayOrderId = document.getElementById("orderId");
+    displayOrderId.textContent = orderId;
 } else {
     window.location.href = "index.html"
 }
 
 const resumeCommande = JSON.parse(localStorage.getItem("dataPannier"));
-console.log(resumeCommande)
+const resumePrise = JSON.parse(localStorage.getItem("totalPrise"));
+const resumeTotalArticles = JSON.parse(localStorage.getItem("totalProduct"))
+console.log(resumePrise + "   " + resumeTotalArticles)
 
 const listeKanapCommande = document.querySelector("#limitedWidthBlock > div >p")
 listeKanapCommande.innerHTML += resumeCommande.map((panier) =>
     `<ul>
 <li>Vous avez commandé <b>${panier.quantity}</b> <b>${panier.name}</b> de couleur <b>${panier.color}</b></li>
-</ul>`
+</ul>
+`
 ).join(" ")
+listeKanapCommande.innerHTML +=
+    `<br>Total d'articles : <b>${resumeTotalArticles}</b>
+     <br>Prix total de la commande : <b>${resumePrise} €</b>`
 
 const listeKanapCommandeStyle = document.querySelectorAll("#limitedWidthBlock > div >p > ul")
 for (let x = 0; x < listeKanapCommandeStyle.length; x++) {
     listeKanapCommandeStyle[x].style.listStyleType = "none";
 }
 
+//Fonction pour le compte a rebour
 function compteRebour(value) {
     setTimeout(() => {
         const buttonReturnToHome = document.querySelector("#order")
@@ -35,7 +42,7 @@ function compteRebour(value) {
         let counter = value;
         let timer = setInterval(() => {
             counter--
-            buttonReturnToHome.value = "Revenir à l'accueil ("+counter+")";
+            buttonReturnToHome.value = "Revenir à l'accueil (" + counter + ")";
             if (counter === 0) {
                 window.location.reload();
                 console.log("over")
@@ -44,14 +51,15 @@ function compteRebour(value) {
                 clearInterval(timer);
             }
         }, 1000);
-    }, 1000)
+    }, 1000);
     return value;
 }
 
+//Construction du button ------
 const buttonReturnToHome = document.querySelector(".confirmation > p")
 buttonReturnToHome.innerHTML +=
-    `<div class="buttonReturnToHomme">
-     <input type="submit" id="order" value="Revenir à l'accueil (${compteRebour(15)})">
+    `<br><div class="buttonReturnToHomme" style="padding-top: 15px">
+     <input type="submit" id="order" value="Revenir à l'accueil (${compteRebour(600)})">
      </div>`
 
 const button = document.querySelector("#limitedWidthBlock > div > p > div > input")
@@ -78,17 +86,24 @@ button.addEventListener("mouseleave", (e) => {
     button.style.boxShadow = "";
 })
 
-const cleanLocalStorageOnClick =[document.querySelector("body > header > div.limitedWidthBlockContainer.menu > div > nav > ul > a:nth-child(2) > li"),
+const cleanLocalStorageOnClick = [document.querySelector("body > header > div.limitedWidthBlockContainer.menu > div > nav > ul > a:nth-child(2) > li"),
     document.querySelector("body > header > div.limitedWidthBlockContainer.menu > div > nav > ul > a:nth-child(1) > li"), document.querySelector("body > header > div.limitedWidthBlockContainer.menu > div > a > img")];
 console.log(cleanLocalStorageOnClick)
 cleanLocalStorageOnClick.forEach(el => {
     console.log(el)
-    el.addEventListener('mouseenter', (e) => {
+    el.addEventListener('click', (e) => {
         console.log(e, "suce")
         window.location.reload();
         localStorage.clear();
     })
 });
+
+//----------------------------------------------
+//Si la page est reload :
+if (performance.navigation.type == performance.navigation.TYPE_RELOAD) {
+        window.location.href = "index.html"
+        localStorage.clear();
+}
 
 setTimeout(() => {
     // localStorage.removeItem("")
